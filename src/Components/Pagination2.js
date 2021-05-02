07cd61a471560942b3c4b775fab14851f99a348f
Mallanagouda Patil <81263900+mallan26@users.noreply.github.com>
2021-05-02 23:13:35 +0530

    Update Pagination2.js

--- src/Components/Pagination2.js
+++ src/Components/Pagination2.js
@@ -9,38 +9,38 @@ const Pagination2 = ({ showPerPage, onPaginationChange, total }) => {
         //   console.log("start value", value - showPerPage);
         //   console.log("end value", value );
         onPaginationChange(value - showPerPage, value)
-    }, [counter])
+             }, [counter])
 
     const onButtonClick = (type) => {
-        if (type === "prev") {
-            if (counter === 1) {
-                setCounter(1)
-            } else {
-                setCounter(counter - 1)
-            }
-        }
-        else if (type === "next") {
-           if(numberOfButtons === counter){
+               if (type === "prev") {
+                if (counter === 1) {
+                     setCounter(1)
+                     } else {
+                   setCounter(counter - 1)
+                 }
+               }
+             else if (type === "next") {
+              if(numberOfButtons === counter){
                setCounter(counter)
-           } else {
-            setCounter(counter + 1)
+                  } else {
+              setCounter(counter + 1)
+              }
+             }
            }
-        }
-    }
 
-    return (
+        return (
         <div className="d-flex justify-content-center">
             <nav aria-label="Page navigation example">
-  <ul class="pagination">
-    <li class="page-item"><a class="page-link" href="!#" onClick={() => onButtonClick('prev')}>Previous</a></li>
-    {
-        new Array(numberOfButtons).fill("").map((ele, i) => (
+             <ul class="pagination">
+             <li class="page-item"><a class="page-link" href="!#" onClick={() => onButtonClick('prev')}>Previous</a></li>
+             {
+              new Array(numberOfButtons).fill("").map((ele, i) => (
              <li class={`page-item ${i+1===counter ? "active" : null}`}><a class="page-link" href="!#" onClick={() => setCounter(i+1)}>{i+1}</a></li>
-        ))
-    }
-    <li class="page-item"><a class="page-link" href="!#"  onClick={() => onButtonClick('next')}>Next</a></li>
-  </ul>
-</nav>
+            ))
+            }
+           <li class="page-item"><a class="page-link" href="!#"  onClick={() => onButtonClick('next')}>Next</a></li>
+          </ul>
+         </nav>
         </div>
     )
 }
